8d4a2ccfbf386cb7b0011d98fe383a086faaccd3
ATepylo <45440409+ATepylo@users.noreply.github.com>
2020-08-27 19:04:10 -0400

    Initial commit

---
+++ Assets/Scripts/Tiles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+    public Transform entry1;
+    public Transform entry2;
+    public Transform centre;
+
+    public Sprite sprite;
+
+    public bool beetIsOn;
+
+    public int maxTurns;
+    public int turns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
