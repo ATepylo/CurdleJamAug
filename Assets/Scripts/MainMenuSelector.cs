f9955b3a02405650969f82cf9257b692c3d14df5
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-08-28 22:23:20 -0700

    holy shit, way too much to write down here

--- Assets/Scripts/MainMenuSelector.cs
+++ Assets/Scripts/MainMenuSelector.cs
@@ -3,25 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEditor;
 
 public class MainMenuSelector : MonoBehaviour
 {
+    //******************THIS IS BROKEN!!**********************
     private Options option;
 
     public Slider rowsSlider;
     public Slider speedSlider;
 
+    [SerializeField] GameObject[] menuButtons;
+    [SerializeField] GameObject pointer;
+    int menuSelection = 0;
     // Start is called before the first frame update
     void Start()
     {
         option = FindObjectOfType<Options>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+        {
+            if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                menuSelection++;
+                pointer.transform.DOMoveY(/*menuButtons[menuSelection-1].transform.position.y*/pointer.transform.position.x -1, 1, false);
+            }
+            else if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                menuSelection--;
+                pointer.transform.DOMoveY(/*menuButtons[menuSelection-1].transform.position.y*/pointer.transform.position.x + 1, 1, false);
+            }
+        }
     }
 
     public void StartRandom()
---
+++ Assets/Transition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class Transition : MonoBehaviour
+{
+    float _transitionSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOLocalMoveY(20, _transitionSpeed, false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //testing the load next scene transition
+        if (Input.GetKeyUp(KeyCode.Space)) transform.DOLocalMoveY(-0.5f, _transitionSpeed, false);
+    }
+}
