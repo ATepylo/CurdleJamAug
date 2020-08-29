def2fa68739e68dc9072152814fac1c3ccd7369c
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-08-28 17:41:03 -0700

    Timer COmplete

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -8,9 +8,16 @@ public class UI : MonoBehaviour
 {
     private int _check;
     [SerializeField] Text _turnsUsed;
+    
     TileSelector _turnAccess;
     int maxTurns;
     PuzzleOneScript pOne;
+
+    //Timer
+    float l_Time = 120;
+    [SerializeField] Text t_Text;
+    [SerializeField] GameObject t_Size;
+    Vector2 t_StartSize;
     // Start is called before the first frame update
     private void OnEnable()
     {
@@ -21,15 +28,54 @@ public class UI : MonoBehaviour
             maxTurns = pOne.getMaxMoves();
             _turnsUsed.text = "Turns Used \n \n" + "<b>" + _check + "</b> /" + maxTurns;
         }
+        t_StartSize = t_Size.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(WaitForStart());
         if (_check != _turnAccess._turns)
         {
             _check = _turnAccess._turns;
             _turnsUsed.text = "Turns Used \n \n" + "<b>" + _check + "</b> /" + maxTurns;
+        }
+    }
+
+    void TimerTextFormat()
+    {
+        l_Time -= Time.deltaTime;
+        if (Mathf.Round(l_Time % 60) >= 10)
+        {
+            t_Text.text = $"{Mathf.Floor((l_Time / 60))}:{Mathf.Floor(l_Time % 60)}";
+        }
+        if (Mathf.Floor(l_Time % 60) < 10)
+        {
+            t_Text.text = $"{Mathf.Round((l_Time / 60))}:0{Mathf.Floor(l_Time % 60)}";
+        }
+        if (l_Time <= 0)
+        {
+            t_Text.text = "Times Up!";
+            
+        }
+        if (l_Time <= 30)
+        {
+            Hurry();
+        }
+    }
+
+    void Hurry()
+    {
+        t_Size.transform.position = Vector3.Lerp(t_StartSize, new Vector3(t_Size.transform.position.x, t_Size.transform.position.y + 5, t_Size.transform.position.z), Mathf.PingPong(Time.time, .5f));
+        t_Text.color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time, 1f));
+
+    }
+    IEnumerator WaitForStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (t_Text != null)
+        {
+            TimerTextFormat();
         }
     }
 }
